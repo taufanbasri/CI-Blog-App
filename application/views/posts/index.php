de934f2f8468eb2f35e58d066f8fc2007857aa88
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-05-02 11:12:51 +0700

    word limit in index show

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -3,7 +3,7 @@
 <?php foreach($posts as $post) : ?>
     <h3><?= $post['title']; ?></h3>
     <small class="post-date">Posted on: <?= $post['created_at'] ?></small> <br>
-    <?= $post['body']; ?>
+    <?= word_limiter($post['body'], 60); ?>
     <br><br>
     <p><a class="btn btn-default" href="<?= site_url('/posts/' . $post['slug']) ?>">Read More</a></p>
 <?php endforeach; ?>
